29abc2e1ba48c0bbb0c3fa8695e2c0a3a2c8be48
Vallek <vforval86@gmail.com>
2021-08-21 17:30:29 +0300

    поменял цель на показ заголовков по фокусу, сохр процесса

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,4 @@
 let boxItems = document.querySelectorAll('.box-item');
-let a = []; 
 
 boxItems.forEach(
 	function changeOnHover(el) {
@@ -7,76 +6,20 @@ boxItems.forEach(
 		let target = el.querySelector('.info');
 		let heading = el.querySelector('.box-item__heading');
 		let itsCode = el.classList.contains("always-show");
-		el.addEventListener('mouseenter', showPopup);
-		el.addEventListener('mouseleave', hidePopup);
+		let boxLink = el.querySelector('.box-item__image-link');
+		el.addEventListener('mouseenter', isInViewport);
+		el.addEventListener('mouseleave', isOutViewport);
 		el.addEventListener('mouseenter', onBox);
 		el.addEventListener('mouseleave', outBox);
-
-		function getSizes() {
-			let boxSide = el.offsetWidth;
-			let viewportWidth = window.innerWidth;
-			let rightSide = viewportWidth - el.getBoundingClientRect().right;
-			let leftSide = el.getBoundingClientRect().left;
-			if (
-				target !== null &&
-				viewportWidth >= 800 &&
-				rightSide >= boxSide ||
-				leftSide >= boxSide
-			) {
-				return boxSide;
-			}
-		}
-		
-		window.addEventListener('resize', getSizes);
-
-		function passSizes(param) {
-			return param;
-		}
-		passSizes(getSizes());
-		let x = passSizes(getSizes());
-		a.push(x);
-		console.log(a);
-
-		function showPopup() {
-			if (
-				target !== null &&
-				a != undefined
-			) {
-				target.classList.remove('visually-hidden');
-				target.classList.add('inside-viewport');
-				let y = a[a.map(x => x * 2)];
-				target.style.left = y + 'px';
-				console.log(y);
-			}
-			else {
-				return;
-			}
-		}
-		
-		function hidePopup() {
-			if (target !== null) {
-				target.classList.remove('out-of-viewport');
-				target.classList.remove('inside-viewport');
-				target.style.left = null;
-				target.style.right = null;
-				target.classList.add('visually-hidden');
-			}
-			else {
-				return;
-			} 
-		}
-
-		if (target !== null) {
-			let removeIt = function remove() {	
-				target.classList.add('visually-hidden');	
-				target.style.left = null;
-				target.style.right = null;
-			};
-			target.addEventListener('mouseover', removeIt);
+		boxLink.addEventListener('focus', onBox);
+		boxLink.addEventListener('focusout', outBox);
+		let boxHeading = boxLink.querySelector('.caption-link');
+		if (boxHeading != null) {
+			boxHeading.addEventListener('focus', onBox);
 		}
 		else {
 			return;
-		}	
+		}
 
 		function onBox() {
 			if (! itsCode) {
@@ -96,5 +39,60 @@ boxItems.forEach(
 			}
 		}
 
+		function isInViewport() {
+			let boxSide = el.offsetWidth;
+			let viewportWidth = window.innerWidth;
+			let rightSide = viewportWidth - el.getBoundingClientRect().right;
+			let leftSide = el.getBoundingClientRect().left;
+			if (
+				target !== null &&
+				rightSide >= boxSide &&
+				viewportWidth >= 800
+			) {
+				target.classList.remove('visually-hidden');
+				target.classList.add('inside-viewport');
+				target.style.left = boxSide + 'px';
+			}
+			else {
+				if (
+					target !== null &&
+					leftSide >= boxSide &&
+					viewportWidth >= 800
+					) {
+					target.classList.remove('visually-hidden');
+					target.classList.add('out-of-viewport');
+					target.style.right = boxSide + 'px';
+				}
+				else if (target !== null) {
+					target.style.left = null;
+					target.style.right = null;
+					return;
+				} 
+			}	
+		}
+		
+		function isOutViewport() {
+			if (target !== null) {
+				target.classList.remove('out-of-viewport');
+				target.classList.remove('inside-viewport');
+				target.style.left = null;
+				target.style.right = null;
+				target.classList.add('visually-hidden');
+			}
+			else {
+				return;
+			} 
+		}
+		if (target !== null) {
+			let removeIt = function remove() {	
+				target.classList.add('visually-hidden');	
+				target.style.left = null;
+				target.style.right = null;
+			};
+			target.addEventListener('mouseover', removeIt);
+		}
+		else {
+			return;
+		}	
 	}
 );
