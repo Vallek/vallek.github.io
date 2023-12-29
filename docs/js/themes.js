832aaf7881fd3e80327b311cccf813d1ec03cce2
Vallek <vforval86@gmail.com>
2023-12-29 10:55:33 +0300

    вынес детект темной темы в инлайн в начало body

--- docs/js/themes.js
+++ docs/js/themes.js
@@ -4,14 +4,6 @@ const page = document.querySelector('.home');
 const lightThemeLink = document.querySelector('.themes__theme_light');
 const darkThemeLink = document.querySelector('.themes__theme_dark');
 
-// Check if user set dark theme before
-if (localStorage.getItem('vallek-portfolio-theme') == 'dark') {
-	page.classList.add('dark');
-}
-// Check if user prefers dark theme
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-	page.classList.add('dark');
-}
 // Check theme preference switch real time
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
 	const newColorScheme = event.matches ? 'dark' : 'light';
