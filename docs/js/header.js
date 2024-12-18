e2950aa740fedaa78ce4962175c1926920e8958c
Vallek <vforval86@gmail.com>
2024-12-18 18:07:14 +0300

    обновил раздел о себе, переделал разметку, убрал лишнее из js и css

--- docs/js/header.js
+++ docs/js/header.js
@@ -2,7 +2,7 @@
 let header = document.querySelector('.header');
 let showButton = header.querySelector('.header__show');
 let title = header.querySelector('.header__title');
-let about = header.querySelector('.header__about-me');
+let about = header.querySelector('.header__about');
 let info = header.querySelector('.header__info');
 let main = header.querySelector('.header__main');
 let intro = header.querySelector('.header__intro');
@@ -31,10 +31,8 @@ function showPopup() {
 			let displayValue = elStyles.getPropertyValue('display');
 			if (displayValue != 'none') {
 				el.classList.toggle('visually-hidden');
-				el.style.display = 'revert';
 			}
 			if (displayValue == 'none') {
-				el.style.display = 'revert';
 				el.classList.remove('visually-hidden');
 			}
 		}
